ddfa8f47bacdf715f24cdd5aa02d3d4210ac359b
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-06-25 18:59:17 +0900

    created ArticleHandler

---
+++ backend/internal/handler/article.go
@@ -0,0 +1,27 @@
+
+type ArticleHandler struct {
+	logger *slog.Logger
+}
+
+func NewArticleHandler(logger *slog.Logger) *ArticleHandler{
+	return &ArticleHandler{logger: logger}
+}
+
+
+// 記事一覧取得
+func (h *ArticleHandler) GetArticles(w http.ResponseWriter, r *http.Request) {
+	h.logger.Info("記事一覧取得リクエスト")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"articles":[]}`)) 
+}
+
+// 記事投稿
+func (h *ArticleHandler) CreateArticle(w http.ResponseWriter, r *http.Request) {
+	h.logger.Info("記事投稿リクエスト")
+	w.Header().Set("Content-type", "application/json")
+	// Write()がWriteHeader(http.StatusOK)を自動で呼ぶから、その前に明示的に呼び出す。
+	w.Header().WriteHeader(http.StatusCreated)
+	// TODO: 配列ではなくarticleをセット
+	w.Write([]byte(`{"articles":[]}`))
+}
