5dd4dbc42bb2ab5b766ac8130e3661c9f6074197
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-06-27 17:09:00 +0900

    created model/user.go

--- backend/internal/model/article.go
+++ backend/internal/model/article.go
@@ -2,14 +2,21 @@ package model
 
 import "time"
 
+// DB保存用のArticleデータ
 type Article struct {
-	ID int64 `json: "id"`
-	UserId int64 `json: "authorId"`
-	Title string `json: "title"`
-	Body string `json: "body"`
-	ImageUrls []string `json: "imageUrls"`
-	LikeCount int `json: "likeCount"`
-	MvpCount int `json: "mvpCount`
-	CreatedAt time.Time `json: "createdAt"`
-	UpdatedAt time.Time `json: "updatedAt"`
+	ID int64 `json:"id"`
+	UserId int64 `json:"authorId"`
+	Title string `json:"title"`
+	Body string `json:"body"`
+	ImageUrls []string `json:"imageUrls"`
+	LikeCount int `json:"likeCount"`
+	MvpCount int `json:"mvpCount`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+// リクエスト用のArticleデータ
+type CreateArticle struct {
+	Title string `json:"title"`
+	Body string `json:"body"`
 }
