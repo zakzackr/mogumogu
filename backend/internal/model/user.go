c2591bfd4f38c01f10843585d8c71b4f4a46c120
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-06-29 14:10:42 +0900

    fix errors

--- backend/internal/model/user.go
+++ backend/internal/model/user.go
@@ -4,16 +4,16 @@ import "time"
 
 // DB保存用のUserデータ
 type User struct {
-	ID int64 `json:"id`
-	Username string `json:"username"`
-	Display_name string `json:"displayName"`
-	Email string `json:"email"`
-	Password string `json:"password"`
-	Bio string `json:"bio"`
-	FollowersCount int `json:"followersCount"`
-	FollowingCount int `json:"followingCount"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	ID             int64     `json:"id"`
+	Username       string    `json:"username"`
+	Display_name   string    `json:"displayName"`
+	Email          string    `json:"email"`
+	Password       string    `json:"password"`
+	Bio            string    `json:"bio"`
+	FollowersCount int       `json:"followersCount"`
+	FollowingCount int       `json:"followingCount"`
+	CreatedAt      time.Time `json:"createdAt"`
+	UpdatedAt      time.Time `json:"updatedAt"`
 }
 
 // TODO: 以下も作成すること
