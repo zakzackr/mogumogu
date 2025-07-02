da1b818fbcc9eb47197f8c13bd58c880b64a4beb
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-07-02 17:54:01 +0900

    modified ui

--- frontend/src/app/articles/[id]/page.tsx
+++ frontend/src/app/articles/[id]/page.tsx
@@ -19,37 +19,53 @@ type ArticleDetailProps = {
  * 記事詳細画面
  * @returns 記事詳細画面
  */
+// export default async function ArticleDetailPage({params} : ArticleDetailProps) {
+//     // Cookieからアクセストークンを取得
+//     const cookieStore = await cookies();
+//     const token = cookieStore.get("accessToken");
+
+//     // TODO: 例外ハンドリングを行い、エラーメッセージの表示
+//     // 記事詳細取得用APIの呼び出し
+//     const articleDetail = await fetchArticleById((await params).id, token?.value);
+
+//     let role: string | undefined = undefined;
+//     let username: string | undefined = undefined;
+//     if (token){
+//         // 秘密鍵のデコード
+//         const jwtSecret = Buffer.from(process.env.JWT_SECRET!, "base64"); 
+//         // JWTをdecodeしてroleを取得
+//         const decoded = jwt.verify(token.value, jwtSecret);
+
+//         if (typeof decoded === "object"){
+//             if ("role" in decoded) {
+//                 role = (decoded as JwtPayload).role as string;
+//             }
+
+//             if ("sub" in decoded){
+//                 username = (decoded as JwtPayload).sub as string;
+//             }
+//         }
+//     }
+
+//     return (
+//         <div>
+//             <Header role={role} username={username}/>
+//             <div className="pt-30 pb-12">
+//                 <ArticleDetail articleDetail={articleDetail} />
+//             </div>
+//         </div>
+//     )
+// }
+
 export default async function ArticleDetailPage({params} : ArticleDetailProps) {
-    // Cookieからアクセストークンを取得
-    const cookieStore = await cookies();
-    const token = cookieStore.get("accessToken");
 
     // TODO: 例外ハンドリングを行い、エラーメッセージの表示
     // 記事詳細取得用APIの呼び出し
-    const articleDetail = await fetchArticleById((await params).id, token?.value);
-
-    let role: string | undefined = undefined;
-    let username: string | undefined = undefined;
-    if (token){
-        // 秘密鍵のデコード
-        const jwtSecret = Buffer.from(process.env.JWT_SECRET!, "base64"); 
-        // JWTをdecodeしてroleを取得
-        const decoded = jwt.verify(token.value, jwtSecret);
-
-        if (typeof decoded === "object"){
-            if ("role" in decoded) {
-                role = (decoded as JwtPayload).role as string;
-            }
-
-            if ("sub" in decoded){
-                username = (decoded as JwtPayload).sub as string;
-            }
-        }
-    }
+    const articleDetail = await fetchArticleById((await params).id);
 
     return (
         <div>
-            <Header role={role} username={username}/>
+            <Header/>
             <div className="pt-30 pb-12">
                 <ArticleDetail articleDetail={articleDetail} />
             </div>
