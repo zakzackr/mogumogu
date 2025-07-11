f098b6a95e8415469c609abb68222dbf3e48be64
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-07-11 17:52:48 +0900

    create supabase client

---
+++ frontend/src/lib/supabase.ts
@@ -0,0 +1,42 @@
+import { createBrowserClient, createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+/**
+ * Supabaseクライアント
+ */
+
+// ブラウザからSupabase APIにアクセスするための設定
+export const createClient = () => {
+    // CCでSupabaseクライアントを生成
+    return createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+}
+
+// Server ComponentからSupabase APIにアクセスするための設定
+export const createServerComponentClient = async () => {
+    const cookieStore = await cookies()
+    
+    // SCでSupabaseクライアントを生成
+    return createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {   
+            cookies: {
+                getAll() {
+                    return cookieStore.getAll()
+                },
+                setAll(cookiesToSet) {
+                    try {
+                        cookiesToSet.forEach(({ name, value, options }) =>
+                            cookieStore.set(name, value, options)
+                        )
+                    } catch {
+                        // Server Componentから呼び出された場合は無視
+                    }
+                },
+            },
+        }
+    )
+}
