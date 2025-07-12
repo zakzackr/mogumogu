cc1092b08ea02f2662cb85874b560de7bb1ab21e
zakzackr <ryuichi12150105syhrh@gmail.com>
2025-07-12 12:02:23 +0900

    create login route handler

--- frontend/src/app/api/auth/login/route.ts
+++ frontend/src/app/api/auth/login/route.ts
@@ -1,3 +1,6 @@
+import { createErrorResponse, ERROR_CODES } from '@/lib/apiResponse';
+import { createServerSideClient } from '@/lib/supabase';
+import { StatusCodes } from 'http-status-codes'
 import { NextResponse } from 'next/server'
 
 /**
@@ -12,11 +15,40 @@ export async function POST(request: Request){
 
         // 入力validation
         if (!email || !password){
-            
+            return createErrorResponse(
+                ERROR_CODES.VALIDATION_ERROR,
+                "メールアドレスとパスワードが必要です",
+                StatusCodes.BAD_REQUEST
+            )
         }
 
-    } catch {
+        // supabaseクライアントを作成
+        const supabase = await createServerSideClient();
 
+        // ログイン
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email, password
+        })
+
+        if (error) {
+            return createErrorResponse(
+                ERROR_CODES.AUTHENTICATION_ERROR,
+                "ログインに失敗しました",
+                StatusCodes.UNAUTHORIZED
+            )
+        }
+
+        // TODO: username, avatar_urlなどの返却を検討
+        return NextResponse.json(
+            { message: "ログインに成功しました"},
+            { status: StatusCodes.OK }
+        )
+    } catch (error) {
+        return createErrorResponse(
+            ERROR_CODES.INTERNAL_SERVER_ERROR,
+            "予期せぬエラーが発生しました",
+            StatusCodes.INTERNAL_SERVER_ERROR
+        )
     }
     // SupabaseのServerSideクライアントを作成？
 
